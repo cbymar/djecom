eb42a681caa4d9ea952561cb42c5a7a4f87f69aa
cbymar <chrisemsbu@gmail.com>
2021-02-08 13:05:15 -0800

    adding featured views

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -18,6 +18,8 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from products.views import ProductListView, product_list_view, \
+    ProductDetailView, product_detail_view
 
 from .views import home_page, about_page, contact_page, login_page, register_page
 
@@ -27,6 +29,10 @@ urlpatterns = [
     path('contact', contact_page),
     path('login/', login_page),
     path('register/', register_page),
+    path('products', ProductListView.as_view()),
+    path('products-fbv', product_list_view),
+    path('products/<int:pk>/', ProductDetailView.as_view()),
+    path('products-fbv/<int:pk>/', product_detail_view),
     path('admin/', admin.site.urls),
 ]
 
--- src/products/models.py
+++ src/products/models.py
@@ -1,7 +1,53 @@
 from django.db import models
+import os
+import random
 
-# Create your models here.
+
+def get_filename_ext(filepath):
+    base_name = os.path.basename(filepath)
+    name, ext = os.path.splitext(base_name)
+    return name, ext
+
+
+def upload_image_path(instance, filename):
+    print(instance)
+    print(filename)
+    new_filename = random.randint(1, 245087)
+    name, ext = get_filename_ext(filename)
+    final_filename = '{new_filename}{ext}'.format(
+        new_filename=new_filename,
+        ext=ext
+    )
+    return "products/{new_filename}/{final_filename}".format(
+        new_filename=new_filename,
+        final_filename=final_filename,
+    )
+
+
+class ProductManager(models.Manager):
+    """
+    We can override methods in this to give bespoke behavior
+    """
+    def get_by_id(self, id):
+        qs = self.get_queryset.filter(id=id)
+        if qs.count() == 1:
+            return qs.first()
+        return None
+
+
 class Product(models.Model):
     title = models.CharField(max_length=20)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=10)
+    image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
+    featured = models.BooleanField(default=False)
+
+    objects = ProductManager()
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def image_url(self):
+        if self.image and hasattr(self.image, "url"):
+            return self.image.url
