4f3625e947f4ed92c67b5aca2a3449ecc306d549
cbymar <chrisemsbu@gmail.com>
2021-02-09 15:32:00 -0800

    just updated html

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -1,22 +1,12 @@
-"""ecommerce URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+ecommerce URL Configuration
+https://docs.djangoproject.com/en/3.1/topics/http/urls/
 """
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 
 from .views import home_page, about_page, contact_page, login_page, register_page
@@ -27,6 +17,7 @@ urlpatterns = [
     path('contact/', contact_page, name="contact"),
     path('login/', login_page, name="login"),
     path('register/', register_page, name="register"),
+    path('bootstrap/', TemplateView.as_view(template_name="bootstrap/example.html")),
     path('products/', include("products.urls", namespace="products")),
     # path('products', ProductListView.as_view()),
     # path('products/<int:pk>/', ProductDetailView.as_view()),
