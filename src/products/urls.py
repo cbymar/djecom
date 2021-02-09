f755ca2a3dbc12bd41be813f4681c430d85271dc
cbymar <chrisemsbu@gmail.com>
2021-02-08 16:13:16 -0800

    adding a products urls file

---
+++ src/products/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from .views import (
+    ProductListView,
+    # product_list_view,
+    ProductDetailView,
+    ProductDetailSlugView,
+    # product_detail_view,
+    # ProductFeaturedListView,
+    # ProductFeaturedDetailView,
+)
+
+urlpatterns = [
+    path('products', ProductListView.as_view()),
+    path('products/<slug:slug>/', ProductDetailSlugView.as_view()),
+]
