4609baa66c2699b1b6490f9f5093778820efc248
cbymar <chrisemsbu@gmail.com>
2021-02-10 10:34:44 -0800

    add tags app

--- src/ecommerce/settings.py
+++ src/ecommerce/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'ecommerce',
     'products',
     'search',
+    'tags',
 ]
 
 MIDDLEWARE = [
--- src/products/models.py
+++ src/products/models.py
@@ -99,9 +99,9 @@ class Product(models.Model):
             return self.image.url
 
 
-def product_pre_stave_receiver(sender, instance, *args, **kwargs):
+def product_pre_save_receiver(sender, instance, *args, **kwargs):
     if not instance.slug:
         instance.slug = unique_slug_generator(instance)
 
 
-pre_save.connect(product_pre_stave_receiver, sender=Product)
+pre_save.connect(product_pre_save_receiver, sender=Product)
