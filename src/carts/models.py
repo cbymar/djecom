4f69cc805cc084a155ee2c72267e5456c6d8b719
cbymar <chrisemsbu@gmail.com>
2021-02-10 17:40:19 -0800

    trying m2m change trigger

--- src/carts/models.py
+++ src/carts/models.py
@@ -1,19 +1,37 @@
 from django.db import models
 from django.conf import settings
-
+from django.db.models.signals import pre_save, post_save, m2m_changed
 from products.models import Product
 
 User = settings.AUTH_USER_MODEL
 
 
 class CartManager(models.Manager):
+
+    def new_or_get(self, request):
+        cart_id = request.session.get("cart_id", None)
+        qs = self.get_queryset().filter(id=cart_id)
+        if qs.count == 1:
+            new_obj = False
+            print("Cart ID exists")
+            cart_obj = qs.first()
+            if request.user.is_authenticated() and cart_obj.user is None:
+                cart_obj.user = request.user
+                cart_obj.save()
+        else:
+            cart_obj = self.new(user=request.user)
+            new_obj = True
+            request.session["cart_id"] = cart_obj.id
+        return cart_obj, new_obj
+
     def new(self, user=None):
         print(user)
         user_obj = None
         if user is not None:
-            if user.is_authenticated():
+            if user.is_authenticated:
                 user_obj = user
         return self.model.objects.create(user=user_obj)
+
 
 class Cart(models.Model):
     user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
@@ -26,3 +44,21 @@ class Cart(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+
+def pre_save_cart_receiver(sender, instance, action, *args, **kwargs):
+    """
+    M2M change:
+    model, m2m field, through
+    Cart model relevant functions to run upon (but before) saving
+    """
+    print(action)
+    products = instance.products.all()
+    total = 0
+    for x in products:
+        total += x.price
+    print(total)
+    instance.total = total
+
+
+m2m_changed.connect(pre_save_cart_receiver, sender=Cart.products.through)
--- src/carts/views.py
+++ src/carts/views.py
@@ -6,23 +6,13 @@ from .models import Cart
 # Cart = apps.get_model("carts", "Cart")
 
 
-def cart_create(user=None):
-    cart_obj = Cart.objects.create(user=None)  # shouldn't this be user=user?
-    print("Creating new cart")
-    return cart_obj
-
-
 def cart_home(request):
-    cart_id = request.session.get("cart_id", None)
-    qs = Cart.objects.filter(id=cart_id)
-    if qs.count == 1:
-        print("Cart ID exists")
-        cart_obj = qs.first()
-        if request.user.is_authenticated() and cart_obj.user is None:
-            cart_obj.user = request.user
-            cart_obj.save()
-    else:
-        # cart_obj = cart_create()  # now that we have a modelManager for Cart, we can:
-        cart_obj = Cart.objects.new(user=request.user)
-        request.session["cart_id"] = cart_obj.id
+    cart_obj, new_obj = Cart.objects.new_or_get(request)
+    # products = cart_obj.products.all()
+    # total = 0
+    # for x in products:
+    #     total += x.price
+    # print(total)
+    # cart_obj.total = total
+    cart_obj.save()
     return render(request, "carts/home.html", {})
